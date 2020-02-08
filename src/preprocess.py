f05c705328ac869eaaf56bcc2a6e6a7bfc832a2f
hardianlawi <hardianlawi@gmail.com>
2020-02-08 17:05:33 +0800

    fix preprocess

--- src/preprocess.py
+++ src/preprocess.py
@@ -7,17 +7,29 @@ from tensorflow.keras.preprocessing.text import Tokenizer
 
 
 class Preprocessor(object):
-    def __init__(self, maxlen, oov_token):
+    def __init__(self, maxlen, vocab_size, oov_token):
+        self.__oov_token = oov_token
         self._tokenizer = Tokenizer(oov_token=oov_token)
+        self._vocab_size = vocab_size
         self._maxlen = maxlen
 
     def fit_on_texts(self, texts):
         self._tokenizer.fit_on_texts(texts)
+        self._word2id = dict(
+            zip(self._tokenizer.index_word.values(), self._tokenizer.index_word.keys())
+        )
+        self.__oov_id = self._word2id[self.__oov_token]
 
     def transform(self, sentences: List[str]):
         sequences = self._tokenizer.texts_to_sequences(sentences)
         padded_sequences = sequence.pad_sequences(sequences, maxlen=self._maxlen)
+        padded_sequences = self._filter_oov(padded_sequences)
         return padded_sequences
+
+    def _filter_oov(self, X):
+        X = X.copy()
+        X[X >= self._vocab_size] = self.__oov_id
+        return X
 
     def save(self, path):
         _make_dir_if_not_exists(path)
