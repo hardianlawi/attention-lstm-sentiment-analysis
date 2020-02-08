c0513c5abc778d6b54b3bb81f6cbd92581a2c7de
hardianlawi <hardianlawi@gmail.com>
2020-02-08 22:22:18 +0800

    set fixed seed

--- src/models.py
+++ src/models.py
@@ -1,6 +1,9 @@
 import tensorflow as tf
+from tensorflow.keras.initializers import GlorotUniform, Orthogonal
 from tensorflow.keras.layers import LSTM, Dense, Embedding, Input, Layer
 from tensorflow.keras.models import Model
+
+tf.random.set_seed(2020)
 
 
 def load_model(model_path):
@@ -17,10 +20,19 @@ def get_model(model_type, *args, **kwargs):
 
 def _lstm_based_model(maxlen, vocab_size, emb_size, hidden_size=32, mask_zero=True):
     inp = Input(shape=[maxlen])
-    emb = Embedding(vocab_size, emb_size, mask_zero=mask_zero)
+    emb = Embedding(
+        vocab_size,
+        emb_size,
+        mask_zero=mask_zero,
+        embeddings_initializer=tf.random_uniform_initializer(seed=2020),
+    )
     x = emb(inp)
-    x = LSTM(hidden_size)(x)
-    out = Dense(1, activation="sigmoid")(x)
+    x = LSTM(
+        hidden_size,
+        kernel_initializer=GlorotUniform(seed=2020),
+        recurrent_initializer=Orthogonal(seed=2020),
+    )(x)
+    out = Dense(1, activation="sigmoid", kernel_initializer=GlorotUniform(seed=2020))(x)
     model = Model(inputs=inp, outputs=out)
     return model
 
@@ -29,11 +41,22 @@ def _attention_based_model(
     maxlen, vocab_size, emb_size, hidden_size=32, attention_hs=16, mask_zero=True
 ):
     inp = Input(shape=[maxlen])
-    emb = Embedding(vocab_size, emb_size, mask_zero=mask_zero)
+    emb = Embedding(
+        vocab_size,
+        emb_size,
+        mask_zero=mask_zero,
+        embeddings_initializer=tf.random_uniform_initializer(seed=2020),
+    )
     x = emb(inp)
-    x, hs, cs = LSTM(hidden_size, return_sequences=True, return_state=True)(x)
+    x, hs, cs = LSTM(
+        hidden_size,
+        return_sequences=True,
+        return_state=True,
+        kernel_initializer=GlorotUniform(seed=2020),
+        recurrent_initializer=Orthogonal(seed=2020),
+    )(x)
     x, weights = BahdanauAttention(attention_hs)(hs, x)
-    out = Dense(1, activation="sigmoid")(x)
+    out = Dense(1, activation="sigmoid", kernel_initializer=GlorotUniform(seed=2020))(x)
     model = Model(inputs=inp, outputs=out)
     model_attention = Model(inputs=inp, outputs=weights)
     return model, model_attention
@@ -42,9 +65,13 @@ def _attention_based_model(
 class BahdanauAttention(Layer):
     def __init__(self, units):
         super(BahdanauAttention, self).__init__()
-        self.W1 = tf.keras.layers.Dense(units)
-        self.W2 = tf.keras.layers.Dense(units)
-        self.V = tf.keras.layers.Dense(1)
+        self.W1 = tf.keras.layers.Dense(
+            units, kernel_initializer=GlorotUniform(seed=2020)
+        )
+        self.W2 = tf.keras.layers.Dense(
+            units, kernel_initializer=GlorotUniform(seed=2020)
+        )
+        self.V = tf.keras.layers.Dense(1, kernel_initializer=GlorotUniform(seed=2020))
 
     def call(self, query, values):
         # query : [batch_size, hidden_size]
